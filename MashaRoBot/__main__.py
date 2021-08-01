d026d44448a306e539abbb04510f5928163a0ae1
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-01 14:24:59 +0530

    Create __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -355,7 +355,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
                  ❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  ❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  ❍ I check for admins' permissions before executing any command and more stuffs
-                 \n_Zaira's licensed under the GNU General Public License v3.0_
+                 \n_KGF's licensed under the GNU General Public License v3.0_
                  Here is the [SUPPORT](https://t.me/noobiezhub).
                  If you have any question about KGF, let us know at @noobiezhub.""",
             parse_mode=ParseMode.MARKDOWN,
